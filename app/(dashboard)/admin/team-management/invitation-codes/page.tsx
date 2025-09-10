005a2b8ff6892fa03042a44512463b208a3c36b3
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-09-11 01:34:04 +0530

    `feat: invitation page `

--- app/(dashboard)/admin/team-management/invitation-codes/page.tsx
+++ app/(dashboard)/admin/team-management/invitation-codes/page.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
 
-const page = () => {
+import prisma from '@/lib/prisma';
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from 'next/navigation';
+import React from 'react'
+import InvitationCodes from '@/components/dashboard/admin/invitation-codes';
+
+const page = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user?.id) {
+    redirect("/")
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user.id
+    },
+    select: {
+      companyId: true, 
+      role: true
+    }
+  })
+
+  if (!user || user.role !== "ADMIN") {
+    redirect("/")
+  }
+if (!user.companyId) {
+  redirect("/")
+}
+
+  const codes = await prisma.code.findMany({
+    where: {
+      companyId: user.companyId
+    },
+    orderBy: {
+      used: "asc"
+    }
+  })
+
   return (
-    <div>invitation codes</div>
+    <InvitationCodes initialCodes={codes} />
   )
 }
 
